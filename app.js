ff1bc4966de7536b0d414efb2363ede05ca44d15
Lanways <mcbluedd@gmail.com>
2023-05-13 14:01:38 +0800

    feat: add index login and register page

--- app.js
+++ app.js
@@ -5,14 +5,45 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const PORT = 3000
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+
+
 app.get('/', (req, res) => {
-  res.send('hello world!!')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then(todos => {
+      return res.render('index', { todos })
+    })
+    .catch(error => {
+      return res.status(422).json(error)
+    })
 })
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
+})
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
