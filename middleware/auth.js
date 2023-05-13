b40f5c9561967c769e90daad2d267af0d4c41bf5
Lanways <mcbluedd@gmail.com>
2023-05-14 02:01:32 +0800

    feat: add user authentication in todos and home routers

---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+  authenticator: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    // req.flash('warning_msg', 'please login then use!')
+    res.redirect('/users/login')
+  }
+}
